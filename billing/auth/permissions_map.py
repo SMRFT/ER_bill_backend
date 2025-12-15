7d907f7b79e7cc2b71913f18ed460d446c60edcc
SMRFT <parthibansmrft@gmail.com>
2025-12-15 12:48:53 +0530

    erreport

--- billing/auth/permissions_map.py
+++ billing/auth/permissions_map.py
@@ -4,6 +4,7 @@ PAGE_MAPPING = {
     '/_b_a_c_k_e_n_d/ERBilling/doctorlist/': 'ER-P-ERDL',
     '/_b_a_c_k_e_n_d/ERBilling/procedurelist/': 'ER-P-ERPL',
     '/_b_a_c_k_e_n_d/ERBilling/get_next_bill_number/': 'ER-P-ERGNBN',
+    '/_b_a_c_k_e_n_d/ERBilling/erreport/': 'ER-P-ERREP',
     r'^/_b_a_c_k_e_n_d/ERBilling/update-status(?:/[^/]+)+/$': 'ER-P-ERUS',
     r'^/_b_a_c_k_e_n_d/ERBilling/printbill/?(\?.*)?$': 'ER-P-ERPB',
     r'^/_b_a_c_k_e_n_d/ERBilling/AccountSummary/?(\?.*)?$': 'ER-P-ERAS',
@@ -19,6 +20,7 @@ PAGE_MAPPING = {
     '/Pharmacy/': 'ER-P-ERP',
     '/AccountSummary/': 'ER-P-ERAS',
     '/printbill/': 'ER-P-ERPB',
+    '/erreport/':'ER-P-ERREP',
     
 }
 
