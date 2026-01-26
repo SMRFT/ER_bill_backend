689155cf40ac78b1076f71bc09ffd2806982c9c1
manibalan <manibalan290@gmail.com>
2026-01-26 14:29:50 +0530

    Update Models

--- billing/models.py
+++ billing/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class ERBilling(models.Model):
     uhid = models.CharField(max_length=50)
     patientname = models.CharField(max_length=150)
-    age = models.IntegerField()
+    age = models.CharField(max_length=50,blank=True,null=True)
     gender = models.CharField(max_length=20,blank=True, null=True)
     phonenumber = models.CharField(max_length=20,blank=True, null=True)
     billnumber = models.CharField(primary_key=True,max_length=50)
