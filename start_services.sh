bfdf92adda8d0dd43be407314d1f96efe5fce0b5
manibalan (SMRFT) <manibalansmrft@gmail.com>
2026-03-09 10:31:37 +0530

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -12,7 +12,7 @@ trap cleanup SIGINT SIGTERM
 
 echo "Starting Django Server on port 2111..."
 # Using nohup to keep it running in the background
-export SECURITY_DISABLED=true && python3 manage.py runserver 0.0.0.0:2111 --settings=ER_bill_backend.settings-prod
+export SECURITY_DISABLED=false && python3 manage.py runserver 0.0.0.0:2111 --settings=ER_bill_backend.settings-prod
 
 echo "Starting Automation Worker..."
 python3 manage.py automate_results
